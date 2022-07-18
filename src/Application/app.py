00375cbf8f800407156c2dbec05c6e1625a77b6d
William Dolan <b1lld0l8n@gmail.com>
2022-07-18 13:26:32 -0600

    July 18 updates
    First draft updates

--- src/Application/app.py
+++ src/Application/app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 
@@ -8,46 +10,48 @@ dataset = st.container()
 modelTrainer = st.container()
 
 @st.cache()
-def make_forecast(series):
+def make_forecast(series, df):
     """
         Makes forecast from series input
         Input: selected from dropdown list (series)
     """
-    if series == 'Ethane':
-        series_name = 'Ethane'
-        title = 'Ethane demand'
-        x_label = 'Date'
+    
+    prophet_df = (df.filter(items=['Month', series]).rename(columns={'Date': 'ds', series: 'y'}))
 
-    prophet_df = (df[series])
-
-
-    plotly_fig = make_forecast(sel_series)
-    st.plotly_chart(plotly_fig)
-
+    title = series + ' demand (thousand bbls_d)'
+    
     model = Prophet()
     model.fit(prophet_df)
     future = model.make_future_dataframe(periods=36)
     forecast = model.predict(future)
 
     fig = plot_plotly(model, forecast)
-    fig.update_layout(title=title, yaxis_title=label, xaxis_title='Date')
+    fig.update_layout(title=title, yaxis_title='kbbld_d', xaxis_title='Date')
 
     return fig
 
 with header:
-    st.title('LighhouseLabs Final Project: Time Series Anlaysis')
-    st.text('This is my project on time series analysis')
+    st.title('LighhouseLabs Final Project:')
+    st.title('Time Series Analysis')
+    # st.text('This is my project on time series analysis')
 
 
 with dataset:
     st.header('EIA Energy Consumption')
-    df = pd.read_csv('EIA volumes.csv', parse_dates=['Month'], index_col=['Month'])
+    df = pd.read_csv('EIA_volumes.csv', parse_dates=['Date'])
     # st.write(df.head())
-    st.line_chart(df, use_container_width=True)
+    
+    sel_series = st.selectbox('Choose a graph to plot:', options=['Ethane', 'Propane', 'Gasoline', 'Jet Fuel'])
+    
+    fig_df = df.filter(items=['Date', sel_series])
+    figp = px.line(fig_df, x='Date', y=sel_series +'kbbld', title=sel_series + ' demand')
+    st.plotly_chart(figp, use_container_width=True)
 
 with modelTrainer:
-    st.header('Series forecasting by FBProphet')
-
-    sel_series = st.selectbox('Choose a graph to forecast:', options=['Ethane', 'Propane', 'Gasoline', 'Jet Fuel'])
-
-    
+    st.header('Series forecasting (FBProphet)')
+ 
+    if st.button('Start Forecast'):
+        st.write('Forecasting...')
+        make_forecast(sel_series, df)
+    else:
+        st.write('Click button to forecast')
